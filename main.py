4bbd0c8da66d94c3a4b1f4fc0407590774eaaf27
ashishmuley1101 <ashishmuley1101@gmail.com>
2023-01-06 12:46:11 +0530

    Creating a dictionary datatype in Python.

---
+++ main.py
@@ -0,0 +1,20 @@
+
+# Creating a dictionary datatype in Python.
+
+# Python dictionary is an ordered collection (starting from Python 3.7) of items. It stores elements
+# in key/value pairs. Here, keys are unique identifiers that are associated with each value.
+
+# Syntax : dictionary_name = { key1 = value1, key2 = value2 , ......}
+
+#  key and value with similar datatype
+capital_city = {"Nepal": "Kathmandu", "Italy": "Rome", "England": "London"}
+print(capital_city)
+# Output
+# {'Nepal': 'Kathmandu', 'Italy': 'Rome', 'England': 'London'}
+
+# dictionary with keys and values of different data types
+numbers = {1: "One", 2: "Two", 3: "Three"}
+print(numbers)
+# Output
+# [3: "Three", 1: "One", 2: "Two"]
+
